bcec581c05c777082412c870366dab9b81aa0c8a
Erica Ford <erica.ford0205@gmail.com>
2024-07-09 13:36:12 -0500

    finished testing the first phase of the game, for the project

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// Declare the computer and human scores
-let computerScore = 0;
-let humanScore = 0;
-
 // Write the logic to get the computer choice
 function getComputerChoice() {
   // Randomly generate one: rock, paper, scissors
@@ -59,13 +55,16 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
   console.log(`Player choice: ${humanChoice}`);
   console.log(`Computer choice: ${computerChoice}`);
+
   // Tie
   if (humanChoice === computerChoice) {
-    return console.log(
+    // Inform player of tie
+    console.log(
       `Tie! ${
         humanChoice[0].toUpperCase() + humanChoice.substring(1)
-      } and ${computerChoice} is a stalemate!`
+      } and ${computerChoice} is a stalemate! No points`
     );
+    return "tie";
   }
 
   // Winning conditions for player
@@ -78,24 +77,78 @@ function playRound(humanChoice, computerChoice) {
   // Test results
   let winCondition = WIN_CONDITIONS[humanChoice];
 
-  // Player wins
+  // Player win
   if (winCondition === computerChoice) {
-    return console.log(
+    // Inform player of win
+    console.log(
       `${
         humanChoice[0].toUpperCase() + humanChoice.substring(1)
       } beats ${computerChoice}! You win!`
     );
+    return "player";
+
+    // Computer win
   } else {
-    return console.log(
+    // Inform player of loss
+    console.log(
       `${
         computerChoice[0].toUpperCase() + computerChoice.substring(1)
       } beats ${humanChoice}! You lose!`
     );
+    return "computer";
   }
 }
 
-const HUMAN_SELECTION = getHumanChoice();
-const COMPUTER_SELECTION = getComputerChoice();
+// Logic for game of 5 rounds
+function playGame() {
+  // Declare playRound and score variables inside playGame
+  // Declare the computer and human scores
+  let computerScore = 0;
+  let humanScore = 0;
+  let gameOver = false;
+  let i = 0;
 
-// Test
-playRound(HUMAN_SELECTION, COMPUTER_SELECTION);
+  // Iterate through the game 5 times, but if game is tied continue until tie breaker
+  do {
+    i++;
+    const HUMAN_SELECTION = getHumanChoice();
+    const COMPUTER_SELECTION = getComputerChoice();
+
+    // Announce the game round
+    if (i > 6 && humanScore === computerScore) {
+      console.log("Tie breaker round!");
+      console.log("May the best player win.");
+      console.log(`Round ${i} start!`);
+    } else {
+      console.log(`Round ${i} start!`);
+    }
+
+    let winner = playRound(HUMAN_SELECTION, COMPUTER_SELECTION);
+
+    // Apply Scores
+    if (winner === "player") {
+      humanScore++;
+    }
+
+    if (winner === "computer") {
+      computerScore++;
+    }
+
+    // Game Over Condition -> Must have one winner no ties!
+    if (i > 5 && humanScore !== computerScore) {
+      gameOver = true;
+    }
+  } while (!gameOver);
+
+  // Determine the game winner
+  if (humanScore > computerScore) {
+    console.log("Player Wins!");
+  } else {
+    console.log("Computer Wins!");
+  }
+  console.log("Final Scores: ");
+  console.log(`Player: ${humanScore}`);
+  console.log(`Computer: ${computerScore}`);
+}
+
+playGame();
